4368c290836f9fe1a6e4bf6cd6ae0a0d48d14238
dodotskiees1 <ivanubanan24@gmail.com>
2025-03-04 14:36:53 +0800

    theme featured with PWA

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react'
+
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
